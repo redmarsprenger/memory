395f1090500b33b0edcb8e347e88246bd443dac1
redmarsprenger <redmarsprenger@gmail.com>
2019-10-09 17:41:46 +0200

    Instellingen pagina. Op de tekst zelf klikken om naar de pagina te gaan

--- Memory/InstellingenPage.xaml.cs
+++ Memory/InstellingenPage.xaml.cs
@@ -20,9 +20,98 @@ namespace Memory
     /// </summary>
     public partial class InstellingenPage : Page
     {
+        string[] theme = new string[]
+        {
+            "pack://application:,,,/Memory;component/Resources/memo.png",
+            "pack://application:,,,/Memory;component/Resources/memo2.png",
+            "pack://application:,,,/Memory;component/Resources/memo3.png",
+        };
+        string[] themeNames = new string[]
+        {
+            "Blauw",
+            "Rood",
+            "Geel",
+        };
         public InstellingenPage()
         {
             InitializeComponent();
+        }
+        private void BtnTerug_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService ns = NavigationService.GetNavigationService(this);
+            ns.Navigate(new Uri("WelkomPage.xaml", UriKind.Relative));
+        }
+
+        private void BtnLeft_Click(object sender, RoutedEventArgs e)
+        {
+            int currentIndex = Array.FindIndex(theme, row => row.Contains(imgTheme.Source.ToString()));
+            string nextElement = GetPreviousElement(theme, currentIndex);
+            imgTheme.Source = new BitmapImage(new Uri(nextElement));
+            lblActiveTheme.Content = themeNames[GetPreviousIndex(theme, currentIndex)];
+        }
+
+        private void BtnRight_Click(object sender, RoutedEventArgs e)
+        {
+            int currentIndex = Array.FindIndex(theme, row => row.Contains(imgTheme.Source.ToString()));
+            string nextElement = GetNextElement(theme, currentIndex);
+            imgTheme.Source = new BitmapImage(new Uri(nextElement));
+            lblActiveTheme.Content = themeNames[GetNextElementIndex(theme, currentIndex)];
+        }
+
+        public string GetNextElement(string[] strArray, int index)
+        {
+            if ((index > strArray.Length - 1) || (index < 0))
+                throw new Exception("Invalid index");
+
+            else if (index == strArray.Length - 1)
+                index = 0;
+
+            else
+                index++;
+
+            return strArray[index];
+        }
+
+        public string GetPreviousElement(string[] strArray, int index)
+        {
+            if ((index > strArray.Length - 1) || (index < 0))
+                throw new Exception("Invalid index");
+
+            else if (index == 0)
+                index = strArray.Length - 1;
+
+            else
+                index--;
+
+            return strArray[index];
+        }
+
+        public int GetNextElementIndex(string[] strArray, int index)
+        {
+            if ((index > strArray.Length - 1) || (index < 0))
+                throw new Exception("Invalid index");
+
+            else if (index == strArray.Length - 1)
+                index = 0;
+
+            else
+                index++;
+
+            return index;
+        }
+
+        public int GetPreviousIndex(string[] strArray, int index)
+        {
+            if ((index > strArray.Length - 1) || (index < 0))
+                throw new Exception("Invalid index");
+
+            else if (index == 0)
+                index = strArray.Length - 1;
+
+            else
+                index--;
+
+            return index;
         }
     }
 }
